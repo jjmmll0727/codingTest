e80c0cc3b41959ecdcab16f03874a47f05d06925
jaemin <jjmmll0727@gmail.com>
2021-05-04 11:15:05 +0900

    BBulJit - brute force

--- BOK/dividedsum_2231.py
+++ BOK/dividedsum_2231.py
@@ -1,22 +1,11 @@
-def counting(num):
-    count = 0
-    total = num
-    if num < 10:
-        return num, 1
-    else:
-        while num > 0:
-            total = total + num % 10
-            num = num // 10
-            count = count + 1
-        return total, count
-
+from sys import stdin
 if __name__ == "__main__":
-    sumof = int(input()) 
-    ans = sumof                         
-    sumof = sumof - len(str(sumof))*9  
-    while True:
-        c, d = counting(sumof)
-        if c == ans:
-            print(sumof)
+    num = int(stdin.readline())
+    ans = 0
+    for i in range(1, num+1):
+        temp = sum(list(map(int, str(i))))
+        if i+temp == num:
+            ans = i
             break
-        sumof = sumof + 1
+        
+    print(ans)
